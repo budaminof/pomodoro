47671a2d0c381a9320dcb91b9baced57fc70510b
bud aminof <budaminof@gmail.com>
2016-06-29 13:35:50 -0600

    can see accelorometer position each time you press the button.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,6 @@ angular.module('app.controllers', [])
   vm.form = {};
   vm.form.submitName = submitName;
 
-
   function submitName (){
     pomodoroFactory.pomodoroName(vm.form.name);
     $state.go("tabsController.pomoforoTimer");
@@ -22,32 +21,37 @@ angular.module('app.controllers', [])
 .controller('pomoforoTimerCtrl', function($scope, pomodoroFactory, $state, $interval, $cordovaVibration, $ionicPlatform, $cordovaDeviceMotion) {
   var vm = this;
   vm.pomodoroName = pomodoroFactory.getPomodoroName();
+  vm.moving = moving;
   vm.time = 0;
 
   var interval = $interval(function () {
     vm.time++;
-    if(vm.time === 10) {
+    if(vm.time === 5) {
       $interval.cancel(interval);
       console.log("vibrating!!");
       // $ionicPlatform.ready(function() {
       //   $cordovaVibration.vibrate(100);
       // });
-      // vm.time = 0;
+      vm.time = 0;
       // $state.go('tabsController.pomodoroBreak', {}, {reload: true});
     }
   },1000)
 
-  document.addEventListener("deviceready", function () {
-      $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
-        vm.X = result.x;
-        vm.Y = result.y;
-        vm.Z = result.z;
-        vm.timeStamp = result.timestamp;
-      }, function(err) {
-        console.log(err);
-        // An error occurred. Show a message to the user
-      });
-    }, false);
+  function moving (){
+    document.addEventListener("deviceready", function () {
+        $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+          vm.X = result.x;
+          vm.Y = result.y;
+          vm.Z = result.z;
+          vm.timeStamp = result.timestamp;
+        }, function(err) {
+          console.log(err);
+          // An error occurred. Show a message to the user
+        });
+      }, false);
+
+  }
+
 
 })
 
@@ -57,7 +61,7 @@ angular.module('app.controllers', [])
 
   var interval = $interval(function () {
     vm.time++;
-    if(vm.time === 5) {
+    if(vm.time === 3) {
       $interval.cancel(interval);
       console.log("vibrating!!");
 
