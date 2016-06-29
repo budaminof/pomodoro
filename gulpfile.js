b2b8f9fc10279d7898a07a9361895aa145fd6a56
bud aminof <budaminof@gmail.com>
2016-06-29 12:25:49 -0600

    staring work on accelorometer

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'watch']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,30 +19,39 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('pomoforoTimerCtrl', function($scope, pomodoroFactory, $state, $interval, $cordovaVibration, $ionicPlatform) {
+.controller('pomoforoTimerCtrl', function($scope, pomodoroFactory, $state, $interval, $cordovaVibration, $ionicPlatform, $cordovaDeviceMotion) {
   var vm = this;
   vm.pomodoroName = pomodoroFactory.getPomodoroName();
   vm.time = 0;
 
   var interval = $interval(function () {
     vm.time++;
-
     if(vm.time === 10) {
       $interval.cancel(interval);
       console.log("vibrating!!");
-
       // $ionicPlatform.ready(function() {
       //   $cordovaVibration.vibrate(100);
       // });
-
-      vm.time = 0;
-      $state.go('tabsController.pomodoroBreak', {}, {reload: true});
+      // vm.time = 0;
+      // $state.go('tabsController.pomodoroBreak', {}, {reload: true});
     }
   },1000)
 
+  document.addEventListener("deviceready", function () {
+      $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+        vm.X = result.x;
+        vm.Y = result.y;
+        vm.Z = result.z;
+        vm.timeStamp = result.timestamp;
+      }, function(err) {
+        console.log(err);
+        // An error occurred. Show a message to the user
+      });
+    }, false);
+
 })
 
-.controller('pomodoroBreakCtrl', function($scope, pomodoroFactory, $state, $interval, $cordovaVibration, $ionicPlatform) {
+.controller('pomodoroBreakCtrl', function($scope, pomodoroFactory, $state, $interval, $cordovaVibration, $ionicPlatform, $cordovaDeviceMotion) {
   var vm = this;
   vm.time = 0;
 
@@ -55,7 +64,6 @@ angular.module('app.controllers', [])
       // $ionicPlatform.ready(function() {
       //   $cordovaVibration.vibrate(100);
       // });
-
       vm.time = 0;
       $state.go("tabsController.pomodoro", {}, {reload: true});
     }
